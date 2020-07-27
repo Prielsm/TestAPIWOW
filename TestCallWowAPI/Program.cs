4bcd90fa6e46ddf0d8da58327789a1f3ce69e812
Benjamin Priels <b.priels@wavenet.be>
2020-07-27 17:27:56 +0200

    Add project files.

---
+++ TestCallWowAPI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestCallWowAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
