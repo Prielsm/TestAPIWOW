f3f865791bef76037c4b265d55fa90f79b3e4dd3
Benjamin Priels <b.priels@wavenet.be>
2020-09-21 18:24:40 +0200

    Retravailler les calls pour faire passer les requetes en asynchrones + ajout d'une construction de requete

--- TestCallWowAPI/Program.cs
+++ TestCallWowAPI/Program.cs
@@ -5,20 +5,25 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
 using TestCallWowAPI.Models;
 
 namespace TestCallWowAPI
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string token = "";
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        static async Task Main(string[] args)
         {
             // Get the access token
-            var token = GetAccessToken("b6b4ab532cb245c28315b1b2c606166b", "6Qw6ncBG8cQJBiPiuD2HihmrIbYUEzqE");
+            token = GetAccessToken("b6b4ab532cb245c28315b1b2c606166b", "6Qw6ncBG8cQJBiPiuD2HihmrIbYUEzqE");
 
-            var res = GetCreatureIndex2(token, "us", "static-us", "en_US");
-            // var auctionFileURL = GetAuctionFileUrl(token, "eu", "Blackhand");
-            // var auctions = GetAuctions(auctionFileURL);
+            var res = await GetCreatureIndex("us", "static-us", "en_US");
 
         }
 
@@ -36,18 +41,6 @@ namespace TestCallWowAPI
             return tokenResponse.access_token;
         }
 
-        public static string GetCreatureIndex(string token, string region, string requiredNamespace, string locale)
-        {
-            var client = new RestClient("https://" + region + ".api.blizzard.com/data/wow/creature-family/index");
-            var request = new RestRequest(Method.GET);
-            request.AddHeader("authorization", $"Bearer {token}");
-            request.AddHeader("namespace", requiredNamespace);
-            request.AddHeader("locale", locale);
-            IRestResponse response = client.Execute(request);
-
-            return request.ToString();
-        }
-
         /// <summary>
         /// Call the creature index directly
         /// </summary>
@@ -56,46 +49,38 @@ namespace TestCallWowAPI
         /// <param name="requiredNamespace">The required namespace.</param>
         /// <param name="locale">The locale.</param>
         /// <returns></returns>
-        public static string GetCreatureIndex2(string token, string region, string requiredNamespace, string locale)
+        public static async Task<string> GetCreatureIndex( string region, string requiredNamespace, string locale)
         {
-            string html = "";
-            string url = @"https://us.api.blizzard.com/data/wow/creature-family/index?namespace=static-us&locale=en_US&access_token=USut9r6uSXBwJKAe0llB0RPJAEhmaWmEky";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.AutomaticDecompression = DecompressionMethods.GZip;
+            UriBuilder uriBuilder = new UriBuilder("https://us.api.blizzard.com/");
+            uriBuilder.Path = $"data/wow/creature-type/index";
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            query["namespace"] = requiredNamespace;
+            query["locale"] = locale;
+            uriBuilder.Query = query.ToString();
 
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            using (Stream stream = response.GetResponseStream())
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                html = reader.ReadToEnd();
-            }
+            var request = CreateHttpRequest(HttpMethod.Get, uriBuilder.Uri);
 
-            return html;
+            var response = httpClient.SendAsync(request).Result;
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return content;
         }
 
-        public static string GetAuctionFileUrl(string token, string region, string realm)
+        /// <summary>
+        /// Creates the HTTP request asynchronous.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        /// <param name="url">The URL.</param>
+        /// <returns>
+        /// The http request message
+        /// </returns>
+        private static HttpRequestMessage CreateHttpRequest(HttpMethod method, Uri url)
         {
-            string fileUrl;
-            var client = new RestClient("https://" + region + ".api.blizzard.com/wow/auction/data/" + realm);
-            var request = new RestRequest(Method.GET);
-            request.AddHeader("cache-control", "no-cache");
-            request.AddHeader("content-type", "application/x-www-form-urlencoded");
-            request.AddHeader("authorization", $"Bearer {token}");
-            request.AddHeader("namespace", $"dynamic-eu");
-            IRestResponse response = client.Execute(request);
+            var message = new HttpRequestMessage(method, url);
+            message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-            var auctionApiResponse = JsonConvert.DeserializeObject<AuctionApiResponse>(response.Content);
-            fileUrl = auctionApiResponse.files.First().url;
-            return fileUrl;
-        }
-
-        public static List<Auction> GetAuctions(string fileUrl)
-        {
-            var client = new RestClient(fileUrl);
-            var request = new RestRequest(Method.GET);
-            IRestResponse response = client.Execute(request);
-
-            return JsonConvert.DeserializeObject<AuctionFileContents>(response.Content).auctions;
+            return message;
         }
     }
 }
