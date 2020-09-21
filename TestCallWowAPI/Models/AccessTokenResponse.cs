4ca50e00414a0130f32a47d93a18f44ac9d8b315
Benjamin Priels <b.priels@wavenet.be>
2020-09-21 12:17:17 +0200

    Make API work

---
+++ TestCallWowAPI/Models/AccessTokenResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCallWowAPI.Models
+{
+    class AccessTokenResponse
+    {
+        public string access_token { get; set; }
+    }
+}
--- TestCallWowAPI/Program.cs
+++ TestCallWowAPI/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using TestCallWowAPI.Models;
 
 namespace TestCallWowAPI
 {
@@ -6,9 +13,81 @@ namespace TestCallWowAPI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Get the access token
+            var token = GetAccessToken("b6b4ab532cb245c28315b1b2c606166b", "6Qw6ncBG8cQJBiPiuD2HihmrIbYUEzqE");
+
+            var res = GetCreatureIndex2(token, "us", "static-us", "en_US");
+            // var auctionFileURL = GetAuctionFileUrl(token, "eu", "Blackhand");
+            // var auctions = GetAuctions(auctionFileURL);
+
         }
 
+        public static string GetAccessToken(string clientId, string clientSecret)
+        {
+            var client = new RestClient("https://eu.battle.net/oauth/token");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddParameter("application/x-www-form-urlencoded", $"grant_type=client_credentials&client_id={clientId}&client_secret={clientSecret}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
 
+            var tokenResponse = JsonConvert.DeserializeObject<AccessTokenResponse>(response.Content);
+
+            return tokenResponse.access_token;
+        }
+
+        public static string GetCreatureIndex(string token, string region, string requiredNamespace, string locale)
+        {
+            var client = new RestClient("https://" + region + ".api.blizzard.com/data/wow/creature-family/index");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("authorization", $"Bearer {token}");
+            request.AddHeader("namespace", requiredNamespace);
+            request.AddHeader("locale", locale);
+            IRestResponse response = client.Execute(request);
+
+            return request.ToString();
+        }
+
+        public static string GetCreatureIndex2(string token, string region, string requiredNamespace, string locale)
+        {
+            string html = "";
+            string url = @"https://us.api.blizzard.com/data/wow/creature-family/index?namespace=static-us&locale=en_US&access_token=USut9r6uSXBwJKAe0llB0RPJAEhmaWmEky";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            return html;
+        }
+
+        public static string GetAuctionFileUrl(string token, string region, string realm)
+        {
+            string fileUrl;
+            var client = new RestClient("https://" + region + ".api.blizzard.com/wow/auction/data/" + realm);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddHeader("authorization", $"Bearer {token}");
+            request.AddHeader("namespace", $"dynamic-eu");
+            IRestResponse response = client.Execute(request);
+
+            var auctionApiResponse = JsonConvert.DeserializeObject<AuctionApiResponse>(response.Content);
+            fileUrl = auctionApiResponse.files.First().url;
+            return fileUrl;
+        }
+
+        public static List<Auction> GetAuctions(string fileUrl)
+        {
+            var client = new RestClient(fileUrl);
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<AuctionFileContents>(response.Content).auctions;
+        }
     }
 }
